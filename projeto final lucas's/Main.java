e0757b27b5ef2adb24f0953d18dd5b1b77de4a59
Lucas Machado <99498132+lukziin@users.noreply.github.com>
2022-05-30 14:01:04 -0300

    Update Main.java

--- projeto final lucas's/Main.java
+++ projeto final lucas's/Main.java
@@ -47,52 +47,52 @@ public class Main {
             
           
           if (r1+r2+r3+r4+r5+r6+r7+r8+r9 <= 17 ){
-              System.out.println(" Você tem um perfil conservador, você prefere segurança e não ter surpresas desagradáveis com seu dinheiro, mesmo que elas sejam temporárias. \n     Por conta disso, evite investir em coisas mais arriscadas. \n Mesmo que em longo prazo o retorno seja maior, os altos e baixos podem afetar muito suas emoções e decisões de investimento. \n Alocação Sugerida: \n 10% em investimentos arriscados (bolsa de valores, fundos que buscam superar o Ibovespa); \n 30% em investimentos moderados (fundos imobiliários, fundos que buscam superar o CDI); \n  60% em investimentos conservadores (títulos pós fixados, CDBs e fundos que buscam superar a inflação");
-            System.out.println("Você sabe por onde começar? Digite 1 para sim e 0 para não.");
+              System.out.println("\n Você tem um perfil conservador, você prefere segurança e não ter surpresas desagradáveis com seu dinheiro, mesmo que elas sejam temporárias. \n     Por conta disso, evite investir em coisas mais arriscadas. \n Mesmo que em longo prazo o retorno seja maior, os altos e baixos podem afetar muito suas emoções e decisões de investimento. \n Alocação Sugerida: \n 10% em investimentos arriscados (bolsa de valores, fundos que buscam superar o Ibovespa); \n 30% em investimentos moderados (fundos imobiliários, fundos que buscam superar o CDI); \n  60% em investimentos conservadores (títulos pós fixados, CDBs e fundos que buscam superar a inflação");
+            System.out.println("\n Você sabe por onde começar? Digite 1 para sim e 0 para não.");
             int s1 = meuObj.nextInt();
                if (s1 == 0){
-                System.out.println("Então fique calmo que nós iremos ajudar, vimos que seu perfil é conservador, então devemos começar com os investimentos em titulos diretos, cdb e derivados, esses tipo de investimento você pode encontrar no proprio site ou aplicativo do banco quee você utiliza, esse é o seu investimento mais importante foque nele!");
-                System.out.println("Agora que você aprendeu investir em renda fixa, podemos começar a parte em que você se expoem mais a volatilidade do mercado, ou seja, bolsa de valores e criptomodas, primeiro será preciso criar uma conta em uma corretora, em criptomodas indicamos a Binance, ja na parte da Bolsa de Valores indicamos a Toro Investimentos.");
-                System.out.print("Como já vimos, para investir em algo de renda fixa você precisa apenas do seu banco, por exemplo o banco inter que ofereçe suporte para cdb, cdi, etc, porém para ações e criptomedas já sera em outros lugares, então vamos mostrar as plataformas que consideramos as melhores e mais seguras corretoras. ");
-                System.out.print("Plataformas para investir em criptomoedas: \n 1 – Binance \n 2 - CoinBase \n 3 - Gate.io \n 4 - FoxBit");
-                System.out.print("Plataformas para investir em ações: \n 1 – Toro Investimentos \n 2 - XP Investimentos  \n 3 - RICO \n 4 - NUINVEST");
-                System.out.println("Agora é só começar seus estudos tanto da plataforma quanto dos seus investimentos, caso preciso de mais algo, nos contate por email  : investcerto@gmail.com");
+                System.out.println("\n Então fique calmo que nós iremos ajudar, vimos que seu perfil é conservador, então devemos começar com os investimentos em titulos diretos, cdb e derivados, esses tipo de investimento você pode encontrar no proprio site ou aplicativo do banco quee você utiliza, esse é o seu investimento mais importante foque nele!");
+                System.out.println("\n Agora que você aprendeu investir em renda fixa, podemos começar a parte em que você se expoem mais a volatilidade do mercado, ou seja, bolsa de valores e criptomodas, primeiro será preciso criar uma conta em uma corretora, em criptomodas indicamos a Binance, ja na parte da Bolsa de Valores indicamos a Toro Investimentos.");
+                System.out.print("\n Como já vimos, para investir em algo de renda fixa você precisa apenas do seu banco, por exemplo o banco inter que ofereçe suporte para cdb, cdi, etc, porém para ações e criptomedas já sera em outros lugares, então vamos mostrar as plataformas que consideramos as melhores e mais seguras corretoras. ");
+                System.out.print("\n Plataformas para investir em criptomoedas: \n 1 – Binance \n 2 - CoinBase \n 3 - Gate.io \n 4 - FoxBit");
+                System.out.print("\n Plataformas para investir em ações: \n 1 – Toro Investimentos \n 2 - XP Investimentos  \n 3 - RICO \n 4 - NUINVEST");
+                System.out.println("\n Agora é só começar seus estudos tanto da plataforma quanto dos seus investimentos, caso preciso de mais algo, nos contate por email  : investcerto@gmail.com");
             }  else {
-                System.out.println("Então vá fundo nos seus estudos!");
+                System.out.println("\n Então vá fundo nos seus estudos!");
             }
         
            
            
         }if (r1+r2+r3+r4+r5+r6+r7+r8+r9 >= 18) {
-              System.out.println("Você tem um perfil moderado, já aceita alguns riscos. Portanto, você deve buscar investimentos que ofereçam maior retorno do que título públicos e renda fixa. \n Porém, evite os que podem ter muitas variações de valor, como ações de companhias menores ou fundos de investimento muito agressivos. \n Alocação Sugerida: \n 30% em investimentos arriscados (bolsa de valores, fundos que buscam superar o Ibovespa); \n 30% em investimentos moderados (fundos imobiliários, fundos que buscam superar o CDI); \n 40% em investimentos conservadores (títulos pós fixados, CDBs e fundos que buscam superar a inflação). " );
-              System.out.println("Você sabe por onde começar? Digite 1 para sim e 0 para não.");
+              System.out.println("\n Você tem um perfil moderado, já aceita alguns riscos. Portanto, você deve buscar investimentos que ofereçam maior retorno do que título públicos e renda fixa. \n Porém, evite os que podem ter muitas variações de valor, como ações de companhias menores ou fundos de investimento muito agressivos. \n Alocação Sugerida: \n 30% em investimentos arriscados (bolsa de valores, fundos que buscam superar o Ibovespa); \n 30% em investimentos moderados (fundos imobiliários, fundos que buscam superar o CDI); \n 40% em investimentos conservadores (títulos pós fixados, CDBs e fundos que buscam superar a inflação). " );
+              System.out.println("\n Você sabe por onde começar? Digite 1 para sim e 0 para não.");
               int s2 = meuObj.nextInt();
               if (s2 == 0){
-                  System.out.println("Então fique calmo que nós iremos ajudar, vimos que seu perfil é moderado, então voce é alguem que quer apenas redimentos legais mas arriscando pouco, podemos indicar pra que você diversifique uniformemente seus investimentos e começe com uma renda fixa, como cdb's, tesouro direto e derivados, dessa forma você pode começar a investir a partir do seu banco sem muita dificuldade.");
-                  System.out.println("Agora que você aprendeu investir em renda fixa, podemos começar a parte em que você se expoem mais a volatilidade do mercado, ou seja, bolsa de valores e criptomodas, primeiro será preciso criar uma conta em uma corretora, em criptomodas indicamos a Binance, ja na parte da Bolsa de Valores indicamos a Toro Investimentos.");
-                  System.out.print("Como já vimos, para investir em algo de renda fixa você precisa apenas do seu banco, por exemplo o banco inter que ofereçe suporte para cdb, cdi, etc, porém para ações e criptomedas já sera em outros lugares, então vamos mostrar as plataformas que consideramos as melhores e mais seguras corretoras. ");
-                  System.out.print("Plataformas para investir em criptomoedas: \n 1 – Binance \n 2 - CoinBase \n 3 - Gate.io \n 4 - FoxBit");
-                  System.out.print("Plataformas para investir em ações: \n 1 – Toro Investimentos \n 2 - XP Investimentos  \n 3 - RICO \n 4 - NUINVEST");
-                  System.out.println("Agora é só começar seus estudos tanto da plataforma quanto dos seus investimentos, caso preciso de mais algo, nos contate por email  : investcerto@gmail.com");
+                  System.out.println("\n Então fique calmo que nós iremos ajudar, vimos que seu perfil é moderado, então voce é alguem que quer apenas redimentos legais mas arriscando pouco, podemos indicar pra que você diversifique uniformemente seus investimentos e começe com uma renda fixa, como cdb's, tesouro direto e derivados, dessa forma você pode começar a investir a partir do seu banco sem muita dificuldade.");
+                  System.out.println("\n Agora que você aprendeu investir em renda fixa, podemos começar a parte em que você se expoem mais a volatilidade do mercado, ou seja, bolsa de valores e criptomodas, primeiro será preciso criar uma conta em uma corretora, em criptomodas indicamos a Binance, ja na parte da Bolsa de Valores indicamos a Toro Investimentos.");
+                  System.out.print("\n Como já vimos, para investir em algo de renda fixa você precisa apenas do seu banco, por exemplo o banco inter que ofereçe suporte para cdb, cdi, etc, porém para ações e criptomedas já sera em outros lugares, então vamos mostrar as plataformas que consideramos as melhores e mais seguras corretoras. ");
+                  System.out.print("\n Plataformas para investir em criptomoedas: \n 1 – Binance \n 2 - CoinBase \n 3 - Gate.io \n 4 - FoxBit");
+                  System.out.print("\n Plataformas para investir em ações: \n 1 – Toro Investimentos \n 2 - XP Investimentos  \n 3 - RICO \n 4 - NUINVEST");
+                  System.out.println("\n Agora é só começar seus estudos tanto da plataforma quanto dos seus investimentos, caso preciso de mais algo, nos contate por email  : investcerto@gmail.com");
                   
                   }else {
-                    System.out.println("Então vá fundo nos seus estudos!");
+                    System.out.println("\n Então vá fundo nos seus estudos!");
               }
          
           
           }if (r1+r2+r3+r4+r5+r6+r7+r8+r9 > 20){
-            System.out.println("Vocé é alguem do tipo que tem sangue frio e lida bem com riscos. \n Se esse é o seu perfil, procure investir seu dinheiro de longo prazo em investimentos com maior potencial de retorno, deixando pelo menos 25% do seu patrimônio em investimentos seguros. \n Alocação Sugerida: \n 60% em investimentos arriscados (bolsa de valores, fundos que buscam superar o Ibovespa); \n 20% em investimentos moderados (fundos imobiliários, fundos que buscam superar o CDI); \n 20% em investimentos conservadores (títulos pós fixados, CDBs e fundos que buscam superar a inflação). \n Perfil Muito Arriscado. Cuidado! Risco pode significar mais retorno, porém, é importante ter uma reserva de dinheiro sempre garantida e à mão para momentos turbulentos.");
-            System.out.println("Você sabe por onde começar? Digite 1 para sim e 0 para não.");
+            System.out.println("\n Vocé é alguem do tipo que tem sangue frio e lida bem com riscos. \n Se esse é o seu perfil, procure investir seu dinheiro de longo prazo em investimentos com maior potencial de retorno, deixando pelo menos 25% do seu patrimônio em investimentos seguros. \n Alocação Sugerida: \n 60% em investimentos arriscados (bolsa de valores, fundos que buscam superar o Ibovespa); \n 20% em investimentos moderados (fundos imobiliários, fundos que buscam superar o CDI); \n 20% em investimentos conservadores (títulos pós fixados, CDBs e fundos que buscam superar a inflação). \n Perfil Muito Arriscado. Cuidado! Risco pode significar mais retorno, porém, é importante ter uma reserva de dinheiro sempre garantida e à mão para momentos turbulentos.");
+            System.out.println("\n Você sabe por onde começar? Digite 1 para sim e 0 para não.");
             int s3 = meuObj.nextInt();
             if (s3 == 0){
-                System.out.println("Então fique calmo que nós iremos ajudar, vimos que seu perfil é arrojado, então devemos começar com os investimentos que te rendam mais dinheiro, acçoes e criptomoedas, primeiro será preciso criar uma conta em uma corretora, em criptomodas indicamos a Binance, ja na parte da Bolsa de Valores indicamos a Toro Investimentos.");
-                System.out.println("Agora que você aprendeu investir em algo mais perigoso mas que te gire mais lucro, você deve aprender a investir em renda fixa também que poderá ser algo que te renda menos mas seja algo mais concreto, mais seguro, esse tipo de renda é possível achar no próprio banco, em site ou aplicativo, lá tera itens como tesouro direto, taca selic, cdb's e nesse tipo que você deve aprofundar seus conhecimentos e começar a investir! ");
-                System.out.print("Como já vimos, para investir em algo de renda fixa você precisa apenas do seu banco, por exemplo o banco inter que ofereçe suporte para cdb, cdi, etc, porém para ações e criptomedas já sera em outros lugares, então vamos mostrar as plataformas que consideramos as melhores e mais seguras corretoras. ");
-                System.out.print("Plataformas para investir em criptomoedas: \n 1 – Binance \n 2 - CoinBase \n 3 - Gate.io \n 4 - FoxBit");
-                System.out.print("Plataformas para investir em ações: \n 1 – Toro Investimentos \n 2 - XP Investimentos  \n 3 - RICO \n 4 - NUINVEST");
-                System.out.println("Agora é só começar seus estudos tanto da plataforma quanto dos seus investimentos, caso preciso de mais algo, nos contate por email  : investcerto@gmail.com");
+                System.out.println("\n Então fique calmo que nós iremos ajudar, vimos que seu perfil é arrojado, então devemos começar com os investimentos que te rendam mais dinheiro, acçoes e criptomoedas, primeiro será preciso criar uma conta em uma corretora, em criptomodas indicamos a Binance, ja na parte da Bolsa de Valores indicamos a Toro Investimentos.");
+                System.out.println("\n Agora que você aprendeu investir em algo mais perigoso mas que te gire mais lucro, você deve aprender a investir em renda fixa também que poderá ser algo que te renda menos mas seja algo mais concreto, mais seguro, esse tipo de renda é possível achar no próprio banco, em site ou aplicativo, lá tera itens como tesouro direto, taca selic, cdb's e nesse tipo que você deve aprofundar seus conhecimentos e começar a investir! ");
+                System.out.print("\n Como já vimos, para investir em algo de renda fixa você precisa apenas do seu banco, por exemplo o banco inter que ofereçe suporte para cdb, cdi, etc, porém para ações e criptomedas já sera em outros lugares, então vamos mostrar as plataformas que consideramos as melhores e mais seguras corretoras. ");
+                System.out.print("\n Plataformas para investir em criptomoedas: \n 1 – Binance \n 2 - CoinBase \n 3 - Gate.io \n 4 - FoxBit");
+                System.out.print("\n Plataformas para investir em ações: \n 1 – Toro Investimentos \n 2 - XP Investimentos  \n 3 - RICO \n 4 - NUINVEST");
+                System.out.println("\n Agora é só começar seus estudos tanto da plataforma quanto dos seus investimentos, caso preciso de mais algo, nos contate por email  : investcerto@gmail.com");
             } else {
-                System.out.println("Então vá fundo nos seus estudos!");
+                System.out.println("\n Então vá fundo nos seus estudos!");
         }
         
           meuObj.close();
